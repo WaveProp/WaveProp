1c1229e7ed82e94195acbb04b08065e7efc05f8f
Luiz Faria <maltezfaria@gmail.com>
2020-10-20 18:02:05 +0200

    add BIE tests to the standard test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,7 @@ using SafeTestsets
 
 @safetestset "Integration" begin include("Integration/runtests.jl") end
 
+@safetestset "BIE" begin include("BIE/runtests.jl") end
+
 @safetestset "IO" begin include("IO/runtests.jl") end
+

1c123efc66ac5b4aa2b2783b75b0f029660aa554
Emile Parolin <emile.parolin@gmail.com>
2020-10-29 17:30:10 +0100

    Add vtkIO

--- src/IO/IO.jl
+++ src/IO/IO.jl
@@ -4,14 +4,18 @@ module IO
     using GmshSDK
     using StaticArrays
     using Printf
+    using WriteVTK
     
     using WaveProp.Geometry    
     using WaveProp.Mesh
     using WaveProp.Utils
+    
+    include("gmshIO.jl")    
+    include("vtkIO.jl")    
 
     export read_geo
     export read_msh
     
-    include("gmshIO.jl")    
+    export vtk_mesh_file
     
 end

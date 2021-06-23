a4fa8a4a985097eb287739eb6ee4b3d5ed4f6451
Luiz M. Faria <maltezfaria@gmail.com>
2021-06-23 21:34:11 +0200

    bring back elastostatic kernels and tests

--- src/Nystrom/elastostatic.jl
+++ src/Nystrom/elastostatic.jl
@@ -1,12 +1,13 @@
 # Single Layer
-function (SL::SingleLayerKernel{T,S})(x,y)::T  where {T,S<:Elastostatic}
-    N = ambient_dimension(S)
-    x==y && return zero(T)
-    μ = SL.op.μ
-    λ = SL.op.λ
+function (SL::SingleLayerKernel{T,S})(target,source)::T  where {T,S<:Elastostatic}
+    N   = ambient_dimension(pde(SL))
+    μ,λ = parameters(pde(SL))
     ν = λ/(2*(μ+λ))
+    x = coords(target)
+    y = coords(source)
     r = x .- y
     d = norm(r)
+    d == 0 && return zero(T)
     RRT = r*transpose(r) # r ⊗ rᵗ
     if N==2
         ID = SMatrix{2,2,Float64,4}(1,0,0,1)
@@ -19,21 +20,22 @@ function (SL::SingleLayerKernel{T,S})(x,y)::T  where {T,S<:Elastostatic}
 end
 
 # Double Layer Kernel
-function (DL::DoubleLayerKernel{T,S})(x,y,ny)::T where {T,S<:Elastostatic}
-    N = ambient_dimension(S)
-    x==y && return zero(T)
-    μ = DL.op.μ
-    λ = DL.op.λ
+function (DL::DoubleLayerKernel{T,S})(target,source)::T where {T,S<:Elastostatic}
+    N = ambient_dimension(pde(DL))
+    μ,λ = parameters(pde(DL))
+    ν = λ/(2*(μ+λ))
+    x = coords(target)
+    y = coords(source)
+    ny = normal(source)
     ν = λ/(2*(μ+λ))
     r = x .- y
     d = norm(r)
+    d == 0 && return zero(T)
     RRT = r*transpose(r) # r ⊗ rᵗ
     drdn = -dot(r,ny)/d
-    dγdny = 1/d*drdn
     if N==2
         ID = SMatrix{2,2,Float64,4}(1,0,0,1)
         return -1/(4π*(1-ν)*d)*(drdn*((1-2ν)*ID + 2*RRT/d^2) + (1-2ν)/d*(r*transpose(ny) - ny*transpose(r)))
-        # return μ/(2*(N-1)*π*(λ+2μ))*( (ID + N*(λ+μ)/(μ*d^2)*RRT)*dγdny - 1/d^N*(r*transpose(ny)-ny*transpose(r)) )
     elseif N==3
         ID = SMatrix{3,3,Float64,9}(1,0,0,0,1,0,0,0,1)
         return -1/(8π*(1-ν)*d^2)*(drdn * ((1-2*ν)*ID + 3*RRT/d^2) + (1-2*ν)/d*(r*transpose(ny) - ny*transpose(r)))
@@ -41,37 +43,45 @@ function (DL::DoubleLayerKernel{T,S})(x,y,ny)::T where {T,S<:Elastostatic}
 end
 
 # Adjoint Double Layer Kernel
-function (ADL::AdjointDoubleLayerKernel{T,S})(x,y,nx)::T where {T,S<:Elastostatic}
-    DL = DoubleLayerKernel{T}(ADL.op)
-    return -DL(x,y,nx) |> transpose
-    # N = ambient_dim(S)
-    # x==y && return zero(T)
-    # μ = ADL.op.μ
-    # λ = ADL.op.λ
-    # ν = λ/(2*(μ+λ))
-    # r = x .- y
-    # d = norm(r)
-    # RRT = r*transpose(r) # r ⊗ rᵗ
-    # if N==2
-    #     ID = Mat{2,2,Float64,4}(1,0,0,1)
-    #     return 1/(4π*(1-ν)*d)*((1-2ν)/d*(nx*transpose(r) + dot(r,nx)*ID - r*transpose(nx)) + 2/d^3*dot(r,nx)*RRT)
-    # elseif N==3
-    #     ID = Mat{3,3,Float64,9}(1,0,0,0,1,0,0,0,1)
-    #     return 1/(8π*(1-ν)*d^2)*((1-2ν)/d*(-nx*transpose(r) + dot(r,nx)*ID + r*transpose(nx)) + 3/d^3*dot(r,nx)*RRT)
-    # end
-end
-
-# Hypersingular kernel
-function (HS::HyperSingularKernel{T,S})(x,y,nx,ny)::T where {T,S<:Elastostatic}
-    N = ambient_dimension(S)
-    x==y && return zero(T)
-    μ = HS.op.μ
-    λ = HS.op.λ
+function (ADL::AdjointDoubleLayerKernel{T,S})(target,source)::T where {T,S<:Elastostatic}
+    # DL = DoubleLayerKernel{T}(pde(ADL))
+    # return -DL(x,y,nx) |> transpose
+    N   = ambient_dimension(pde(ADL))
+    μ,λ = parameters(pde(ADL))
+    ν = λ/(2*(μ+λ))
+    x = coords(target)
+    nx = normal(target)
+    y = coords(source)
     ν = λ/(2*(μ+λ))
     r = x .- y
     d = norm(r)
+    d == 0 && return zero(T)
     RRT = r*transpose(r) # r ⊗ rᵗ
-    drdn    = dot(r,ny)/d
+    drdn = -dot(r,nx)/d
+    if N==2
+        ID = SMatrix{2,2,Float64,4}(1,0,0,1)
+        return -1/(4π*(1-ν)*d)*(drdn*((1-2ν)*ID + 2*RRT/d^2) + (1-2ν)/d*(r*transpose(nx) - nx*transpose(r)))
+    elseif N==3
+        ID = SMatrix{3,3,Float64,9}(1,0,0,0,1,0,0,0,1)
+        out =  -1/(8π*(1-ν)*d^2)*(drdn * ((1-2*ν)*ID + 3*RRT/d^2) + (1-2*ν)/d*(r*transpose(nx) - nx*transpose(r)))
+        return -transpose(out)
+    end
+end
+
+# Hypersingular kernel
+function (HS::HyperSingularKernel{T,S})(target,source)::T where {T,S<:Elastostatic}
+    N = ambient_dimension(pde(HS))
+    μ,λ = parameters(pde(HS))
+    ν = λ/(2*(μ+λ))
+    x = coords(target)
+    nx = normal(target)
+    y = coords(source)
+    ny = normal(source)
+    r = x .- y
+    d = norm(r)
+    d == 0 && return zero(T)
+    RRT   = r*transpose(r) # r ⊗ rᵗ
+    drdn  = dot(r,ny)/d
     if N==2
         ID = SMatrix{2,2,Float64,4}(1,0,0,1)
         return μ/(2π*(1-ν)*d^2)* (2*drdn/d*( (1-2ν)*nx*transpose(r) + ν*(dot(r,nx)*ID + r*transpose(nx)) - 4*dot(r,nx)*RRT/d^2 ) +
--- test/Nystrom/dim_test.jl
+++ test/Nystrom/dim_test.jl
@@ -4,6 +4,7 @@ using WaveProp.Nystrom
 using WaveProp.Geometry
 using WaveProp.Integration
 using WaveProp.Mesh
+using StaticArrays
 
 @testset "Greens interpolant test" begin
     # construct interior solution
@@ -13,9 +14,9 @@ using WaveProp.Mesh
         u    = (qnode) -> SingleLayerKernel(pde)(xout,qnode)
         dudn = (qnode) -> DoubleLayerKernel(pde)(xout,qnode)
         clear_entities!()
-        Ω,M  = WaveProp.IO.gmsh_sphere(dim=2,h=0.2)
+        Ω,M  = WaveProp.IO.gmsh_sphere(dim=2,h=0.4)
         Γ    = boundary(Ω)
-        mesh = NystromMesh(M,Γ;order=2)
+        mesh = NystromMesh(M,Γ;order=4)
         γ₀u   = γ₀(u,mesh)
         γ₁u   = γ₁(dudn,mesh)
         S     = SingleLayerOperator(pde,mesh)
@@ -42,37 +43,37 @@ using WaveProp.Mesh
         @test 10*norm(e1,Inf) < norm(e0,Inf)
         @test norm(e1,Inf) < 1e-2
     end
-#     @testset "Elastostatic" begin
-#         pde  = Elastostatic(dim=3,μ=1,λ=2.)
-#         xout = SVector(3,3,3)
-#         c    = ones(Nystrom.default_density_eltype(pde))
-#         u    = (x)   -> SingleLayerKernel(pde)(xout,x)*c
-#         dudn = (x,n) -> transpose(DoubleLayerKernel(pde)(xout,x,n))*c
-#         Geometry.clear!()
-#         Ω,M  = WaveProp.IO.gmsh_sphere(dim=2,h=0.2)
-#         Γ    = boundary(Ω)
-#         mesh = NystromMesh(view(M,Γ);order=2)
-#         γ₀u   = γ₀(u,mesh)
-#         γ₁u   = γ₁(dudn,mesh)
-#         S     = SingleLayerOperator(pde,mesh)
-#         D     = DoubleLayerOperator(pde,mesh)
-#         e0    = WaveProp.Nystrom.error_interior_green_identity(S,D,γ₀u,γ₁u)
-#         Sdim  = Nystrom.assemble_dim(S)
-#         Ddim  = Nystrom.assemble_dim(D)
-#         e1    = WaveProp.Nystrom.error_interior_green_identity(Sdim,Ddim,γ₀u,γ₁u)
-#         @test 10*norm(norm.(e1,Inf),Inf) < norm(norm.(e0,Inf),Inf)
-#         S,D   = Nystrom.single_doublelayer_dim(pde,mesh)
-#         e2    = WaveProp.Nystrom.error_interior_green_identity(S,D,γ₀u,γ₁u)
-#         @test e1 ≈ e2
-#         K     = AdjointDoubleLayerOperator(pde,mesh)
-#         H     = HyperSingularOperator(pde,mesh)
-#         e0    = WaveProp.Nystrom.error_interior_derivative_green_identity(K,H,γ₀u,γ₁u)
-#         Kdim  = Nystrom.assemble_dim(K)
-#         Hdim  = Nystrom.assemble_dim(H)
-#         e1    = WaveProp.Nystrom.error_interior_derivative_green_identity(Kdim,Hdim,γ₀u,γ₁u)
-#         @test 10*norm(norm(e1,Inf),Inf) < norm(norm(e0,Inf),Inf)
-#         @test norm(norm(e1,Inf),Inf) < 1e-4
-#     end
+    @testset "Elastostatic" begin
+        pde  = Elastostatic(dim=3,μ=1,λ=2.)
+        xout = SVector(3,3,3)
+        c    = ones(Nystrom.default_density_eltype(pde))
+        u    = (dof)   -> SingleLayerKernel(pde)(xout,dof)*c
+        dudn = (dof) -> transpose(DoubleLayerKernel(pde)(xout,dof))*c
+        clear_entities!()
+        Ω,M  = WaveProp.IO.gmsh_sphere(dim=2,h=0.4)
+        Γ    = boundary(Ω)
+        mesh = NystromMesh(view(M,Γ);order=4)
+        γ₀u   = γ₀(u,mesh)
+        γ₁u   = γ₁(dudn,mesh)
+        Sop     = SingleLayerOperator(pde,mesh)
+        Dop     = DoubleLayerOperator(pde,mesh)
+        e0    = WaveProp.Nystrom.error_interior_green_identity(Sop,Dop,γ₀u,γ₁u) / norm(norm.(γ₀u,Inf),Inf)
+        Sdim  = Nystrom.assemble_dim(Sop)
+        Ddim  = Nystrom.assemble_dim(Dop)
+        e1    = WaveProp.Nystrom.error_interior_green_identity(Sdim,Ddim,γ₀u,γ₁u) / norm(norm.(γ₀u,Inf),Inf)
+        @test 10*norm(norm.(e1,Inf),Inf) < norm(norm.(e0,Inf),Inf)
+        Sdim2,Ddim2   = Nystrom.single_doublelayer_dim(pde,mesh)
+        e2    = WaveProp.Nystrom.error_interior_green_identity(Sdim2,Ddim2,γ₀u,γ₁u) /norm(norm.(γ₀u,Inf),Inf)
+        @test e1 ≈ e2
+        Kop     = AdjointDoubleLayerOperator(pde,mesh)
+        Hop     = HyperSingularOperator(pde,mesh)
+        e0    = WaveProp.Nystrom.error_interior_derivative_green_identity(Kop,Hop,γ₀u,γ₁u) / norm(norm.(γ₁u,Inf),Inf)
+        Kdim  = Nystrom.assemble_dim(Kop)
+        Hdim  = Nystrom.assemble_dim(Hop)
+        e1    = WaveProp.Nystrom.error_interior_derivative_green_identity(Kdim,Hdim,γ₀u,γ₁u) / norm(norm.(γ₁u,Inf),Inf)
+        @test 10*norm(norm(e1,Inf),Inf) < norm(norm(e0,Inf),Inf)
+        @test norm(norm(e1,Inf),Inf) < 1e-2
+    end
 #     @testset "Maxwell" begin
 #         pde  = Maxwell(dim=3,k=1.0)
 #         xout = SVector(3,3,3)
